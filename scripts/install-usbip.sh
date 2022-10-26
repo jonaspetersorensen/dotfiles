29e32aeeda96e2234d4e341009ca34bf8cc19d02
jonaspetersorensen <jonas.peter.sorensen@gmail.com>
2022-10-26 09:12:26 +0200

    Add install for usbip

---
+++ scripts/install-usbip.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Install usbip..."
+echo "------------------------------------"
+sudo apt install linux-tools-virtual hwdata
+sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
+echo "------------------------------------"
+echo "Done."
+
