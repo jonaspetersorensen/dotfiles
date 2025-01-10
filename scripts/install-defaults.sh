3e02445820e30bc492f9753b09fd3af3a5f64cf9
jonaspetersorensen <jonas.peter.sorensen@gmail.com>
2025-01-10 18:54:03 +0100

    Remove redundant usbipd tooling

--- scripts/install-defaults.sh
+++ scripts/install-defaults.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # USB/IP info: https://github.com/dorssel/usbipd-win/wiki/WSL-support 
+# As of v4.0.0 you no longer have to install any client-side tooling.
+# Pre v4 info: 
 # - WSL client tools: linux-tools-virtual hwdata
+# - Update usb/ip client tool:
+#   sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
 
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -12,6 +16,6 @@ fi
 
 echo "Install tools and libs..."
 echo "------------------------------------"
-sudo apt -qq install unzip jq build-essential tmux keychain linux-tools-virtual hwdata x11-xserver-utils usbutils git-lfs
+sudo apt -qq install unzip jq build-essential tmux keychain x11-xserver-utils usbutils git-lfs
 echo "------------------------------------"
 echo "All done!"
--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -6,8 +6,6 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
-# Update usb/ip client tool
-sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
 echo "------------------------------------"
 echo "Done."
 
