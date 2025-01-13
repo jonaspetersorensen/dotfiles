9de841f268b002deebdf84d738e72d9ada96a641
jonaspetersorensen <jonas.peter.sorensen@gmail.com>
2025-01-13 09:53:02 +0100

    Remove redundant usb comments

--- scripts/install-defaults.sh
+++ scripts/install-defaults.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# USB/IP info: https://github.com/dorssel/usbipd-win/wiki/WSL-support 
-# As of v4.0.0 you no longer have to install any client-side tooling.
-# Pre v4 info: 
-# - WSL client tools: linux-tools-virtual hwdata
-# - Update usb/ip client tool:
-#   sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
-
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 file="${WORK_DIR}/update-all.sh"
