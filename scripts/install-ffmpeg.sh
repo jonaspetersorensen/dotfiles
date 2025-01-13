536a01cba0c0a91a9366332cc036a6bf10ec06d3
Jonas Peter Sørensen <jonas.peter.sorensen@gmail.com>
2025-01-13 16:50:03 +0100

    work in progress

---
+++ scripts/install-ffmpeg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## PURPOSE
+# 
+# Install or update ffmpeg on WSL with latest static binary due to disto packages are always outdated
+
+## DOCS
+# - https://www.johnvansickle.com/ffmpeg/faq/
+
+
+# Latest
+STATIC_BIN_URL="https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz"
