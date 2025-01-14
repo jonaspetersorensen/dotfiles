840e427230a0078a929ad71b538d6b3de0940f69
Jonas Peter Sørensen <jonas.peter.sorensen@gmail.com>
2025-01-14 17:12:17 +0100

    Done

--- scripts/install-ffmpeg.sh
+++ scripts/install-ffmpeg.sh
@@ -8,5 +8,68 @@
 # - https://www.johnvansickle.com/ffmpeg/faq/
 
 
-# Latest
-STATIC_BIN_URL="https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz"
+VERSION_NAME="ffmpeg-git-amd64-static"
+LOCAL_NAME="ffmpeg"
+TAR_FILENAME="${VERSION_NAME}.tar.xz"
+DOWNLOAD_URL="https://johnvansickle.com/ffmpeg/builds/${TAR_FILENAME}"
+BIN_DIR="$HOME/.local"
+TMP_DIR="$HOME/tmp"
+
+DIALOG_MARGIN="   "
+HEADER_PREFIX="-- "
+DIALOG_SUBLIST_MARKER="${DIALOG_MARGIN}+ "
+
+function delete_previous_version() {
+    if [ -d "${BIN_DIR}/${LOCAL_NAME}" ]; then     
+        rm -rf "${BIN_DIR}/${LOCAL_NAME}"
+    fi
+}
+
+function cleanup() {
+    if [ -f "${TMP_DIR}/${TAR_FILENAME}" ]; then
+        rm -rf "${TMP_DIR}/${TAR_FILENAME}"
+    fi
+    if [ -d "${TMP_DIR}/${LOCAL_NAME}" ]; then
+        rm -rf "${TMP_DIR}/${LOCAL_NAME}"
+    fi
+}
+
+function install_static_bin() {    
+    mkdir -p "${TMP_DIR}/${LOCAL_NAME}"
+    
+    cd "${TMP_DIR}"
+    curl -OL "${DOWNLOAD_URL}"
+    # The content of the tar file is a directory which we do not want, hence --strip-components=1 to go 1 level deeper and extract the contents into our target dir
+    tar -xf "${TAR_FILENAME}" -C "${TMP_DIR}/${LOCAL_NAME}" --strip-components=1
+
+    # CD back
+    cd -;
+
+    mv "${TMP_DIR}/${LOCAL_NAME}" "${BIN_DIR}/"
+
+}
+
+
+
+
+############################################################################################################
+## MAIN
+
+printf "%b\n" "-------------------------------------------------------"
+printf "%b\n" "-- (Re)Install ffmpeg static binary --"
+printf "%b\n" "   "
+
+printf "%b" "${HEADER_PREFIX}Remove old version:"
+delete_previous_version
+printf "%b\n" "${DIALOG_MARGIN}Done."
+
+printf "%b" "${HEADER_PREFIX}Installing ${VERSION_NAME}:"
+install_static_bin
+printf "%b\n" "${DIALOG_MARGIN}Done."
+
+printf "\n%b" "${HEADER_PREFIX}Clean up install:"
+cleanup
+printf "%b\n" "${DIALOG_MARGIN}Done."
+
+
+printf "\n%b\n" "${DIALOG_MARGIN}SCRIPT DONE!"
